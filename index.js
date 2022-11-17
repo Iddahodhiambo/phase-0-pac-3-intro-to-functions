56d480ed18e65c0eff8174d4256b94bebdd1d1f1
Iddahodhiambo <iddahawuor@yahoo.com>
2022-11-17 12:24:52 +0300

    tester

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Follow along with the examples here
+function doNothing() {}
+
+function sayHello() {
+    console.log("Hello!");
+  }
+  sayHello();
+  
+  function sayHelloToIsabel() {
+    console.log("Hello, Isabel!");
+    
+  }
+  sayHelloToIsabel()
+  
+  function sayHelloToSofia() {
+    console.log("Hello, Sofia!");
+  }
+  sayHelloToSofia()
+
+  function sayHelloToBrendan() {
+    console.log("Hello, Brendan!");
+  }
+   sayHelloToBrendan() 
+   
+   function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`)
+   }
+    
+    say("Goodbye", "Julio");
+    say("Julio", "hello");
+
+    function add(x, y) {
+        return x + y;
+      }
+      console.log(add(80, 9000));
+      
+      function say(greeting, firstName) {
+        return `${greeting}, ${firstName}!`;
+        console.log("I was called!");
+      }
+
+     console.log(say("Howdy", "partner"));
+     function say(greeting, firstName) {
+        console.log("I was called!");
+        return `${greeting}, ${firstName}!`;
+      }
+      
+
+
+
+
